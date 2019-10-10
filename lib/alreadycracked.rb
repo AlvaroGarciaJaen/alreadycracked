37143ad9f4c339d48ca6c749fbd5e707a2cce7e2
Alvaro Garcia Jaen <garciajaenalvaro@gmail.com>
2019-10-10 02:02:45 +0200

    Se añade funcionalidad al microservicio. Closes #19

--- lib/alreadycracked.rb
+++ lib/alreadycracked.rb
@@ -11,4 +11,23 @@ class AlreadyCracked
       Digest::SHA256.hexdigest plain
     end
   end
+
+  def detect_digest(hash)
+    case hash.length
+    when 32
+      'md5'
+    when 40
+      'sha1'
+    when 64
+      'sha256'
+    end
+  end
+
+  def get_plain(hash)
+    hashes = {'fc5e038d38a57032085441e7fe7010b0' => 'helloworld',
+              '6adfb183a4a2c94a2f92dab5ade762a47889a5a1' => 'helloworld',
+              '936a185caaa266bb9cbe981e9e05cb78cd732b0b3280eb944412bb6f8f8f07af' => 'helloworld'
+              }
+    hashes[hash]
+  end
 end
