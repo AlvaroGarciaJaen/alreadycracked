22efe6be8dc0403a5994e84e055c8ae965e3fbbf
Alvaro Garcia Jaen <alvaro@alvaro.network>
2019-11-25 20:58:08 +0100

    Se añaden nuevos tests para comprobar /status y PUT. Closes #49

--- t/test_func.rb
+++ t/test_func.rb
@@ -26,6 +26,10 @@ class AlreadyCrackedFuncTest < Test::Unit::TestCase
     assert_equal last_response.content_type, 'application/json'
     body = JSON last_response.body
     assert_equal 'OK', body['status']
+    get body['ejemplo']['ruta']
+    example = JSON last_response.body
+    assert_equal body['ejemplo']['valor']['hash'], example['hash']
+    assert_equal body['ejemplo']['valor']['plain'], example['plain']
   end
 
   def test_hash
@@ -53,6 +57,29 @@ class AlreadyCrackedFuncTest < Test::Unit::TestCase
     assert_equal last_response.content_type, 'application/json'
     body = JSON last_response.body
     assert_equal 'Hash not found', body['message']
+  end
+
+  def test_new_hash
+    get '/crack/21232f297a57a5a743894a0e4a801fc3'
+    assert last_response.not_found?
+    assert_equal last_response.content_type, 'application/json'
+    body = JSON last_response.body
+    assert_equal 'Hash not found', body['message']
+
+    put '/hash/md5/admin'
+    assert last_response.ok?
+    assert_equal last_response.content_type, 'application/json'
+    body = JSON last_response.body
+    assert_equal 'md5', body['type']
+    assert_equal 'admin', body['plain']
+    assert_equal '21232f297a57a5a743894a0e4a801fc3', body['hash']
+
+    get '/crack/21232f297a57a5a743894a0e4a801fc3'
+    assert last_response.ok?
+    assert_equal last_response.content_type, 'application/json'
+    body = JSON last_response.body
+    assert_equal '21232f297a57a5a743894a0e4a801fc3', body['hash']
+    assert_equal 'admin', body['plain']
   end
 
   def test_not_found_hash_type
